1bd2f361160090a4ac5fcc9d671caa1bad6e607b
Muhammad-Bilal-Dev <muhammadbilalasgharali@gmail.com>
2022-12-02 20:06:51 +0500

    PDS-8 Add User Component

---
+++ src/components/Users/index.jsx
@@ -0,0 +1,58 @@
+import "./User.css";
+import { useContext } from "react";
+import { userContext } from "../../App";
+
+const Users = () => {
+  const { users } = useContext(userContext);
+  return (
+    <>
+      <footer id="site-footer">
+        <div className="container clearfix">
+          <div className="left">
+            <h2 className="subtotal">Users</h2>
+            <input className="search" placeholder="search user" />
+            <h3 className="tax">List of all user who are friend your</h3>
+          </div>
+          <div className="right">
+            <h1 className="total">
+              Total: <span>{users.length}</span>
+            </h1>
+            <p className="btn">Create User</p>
+          </div>
+        </div>
+      </footer>
+      <div>
+        {users?.map((user, index) => {
+          return (
+            <div key={index} className="container">
+              <section id="cart">
+                <article className="product">
+                  <header>
+                    <div className="remove">
+                      <h3>Remove User</h3>
+                    </div>
+                  </header>
+                  <div className="content">
+                    <h1>{user.name}</h1>
+                    id: {user.id}A good person is someone who gets paid to work
+                    for a person or company. Workers don't need to work full
+                    time to be considered employees—they simply need to be paid
+                    to work by an employer (the person or business that pays
+                    them).
+                  </div>
+                  <footer className="content">
+                    <span className="qt email">{user.email}</span>
+                    <h2 className="full-price">{user.phone}</h2>
+                    <h2 className="price">Phone</h2>
+                  </footer>
+                </article>
+              </section>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Users;
