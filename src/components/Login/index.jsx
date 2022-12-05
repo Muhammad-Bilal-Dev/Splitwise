ccb6250f7da6fa9c9b30531ca1ad0bd9ceb554c8
Muhammad-Bilal-Dev <muhammadbilalasgharali@gmail.com>
2022-12-05 20:47:13 +0500

    PDS-10 ads functionality to create users in firebase users db

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,20 +1,56 @@
+import { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+
+import { db } from "../../firebase-config";
+
 import "./Login.css"
 
 const Login = () => {
+  const [ data, setData ] = useState(
+    {
+      name: "",
+      phone: "",
+      username: "",
+      email: "",
+      password: ""
+    });
+
+  const changeHandlerData = (event) => {
+    const { name, value } = event.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const createUserToFirebaseDB = async (event) => {
+    console.log("User Creating")
+    event.preventDefault()
+    await addDoc(collection(db, "users"), {
+      name: data.name,
+      phone: data.phone,
+      username: data.username,
+      email: data.email,
+      password: data.password
+    }).then((response) => {
+      console.log("User Created", response)
+      setData({ name: "", phone: "", username: "", email: "", password: "" });
+    }).catch((error) => {
+      console.log("Error: while creating user.", error.message)
+    });
+  }
+
   return (
     <>
       <div className="main_head">
         <div className="main">  	
           <input type="checkbox" id="chk" aria-hidden="true" />
             <div className="signup">
-              <form>
+              <form onSubmit={ createUserToFirebaseDB }>
                 <label htmlFor="chk" aria-hidden="true">Sign up</label>
-                <input type="text" name="name" placeholder="Name" required="" />
-                <input type="text" name="phone" placeholder="Phone" required="" />
-                <input type="text" name="username" placeholder="User name" required="" />
-                <input type="email" name="email" placeholder="Email" required="" />
-                <input type="password" name="pswd" placeholder="Password" required="" />
-                <button>Sign up</button>
+                <input onChange={ changeHandlerData } value={ data.name } type="text" name="name" placeholder="Name" required="" />
+                <input onChange={ changeHandlerData } value={ data.phone } type="text" name="phone" placeholder="Phone" required="" />
+                <input onChange={ changeHandlerData } value={ data.username } type="text" name="username" placeholder="User name" required="" />
+                <input onChange={ changeHandlerData } value={ data.email } type="email" name="email" placeholder="Email" required="" />
+                <input onChange={ changeHandlerData } value={ data.password } type="password" name="password" placeholder="Password" required="" />
+                <button typye="submit">Sign up</button>
               </form>
             </div>
             <div className="login">
